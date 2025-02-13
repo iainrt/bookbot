200f87e3629ce7827112e6dcea64024985535d12
iainrt <iainrt@gmail.com>
2025-02-13 22:02:30 +0000

    Character count function added

--- main.py
+++ main.py
@@ -9,14 +9,26 @@ def word_count(text):
     words = text.split()
     return len(words)
 
+def char_count(text):
+    lower_chars = text.lower()
+    output = {}
+    for char in lower_chars:
+        if char not in output:
+            output[char] = 1
+        else:
+            output[char] += 1
+    return output
+         
 
 path_to_file = "books/frankenstein.txt"
 
 def main(path_to_file):
     text = get_book_text(path_to_file)
-    print(text)
-    count = word_count(text)
-    print(f"word count is {count}")
+    #print(text)
+    #count = word_count(text)
+    #print(f"word count is {count}")
+    input = char_count(text)
+    print(input)
 
 main(path_to_file)
     
